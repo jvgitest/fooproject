10b12fbc01e4036367e4757b69166934937734cd
msow <msow>
2016-05-08 01:16:07 +0000

    deploiments 1

---
+++ sites/all/themes/sky/templates/titi.tpl.php
@@ -0,0 +1,4 @@
+
+<div style="color:magenta">
+ myvar_tpl and (<?php print $variables['myvar']; ?> or <?php print $variables['myvar2'];?> or <?php print $variables['myvar3']; ?>)
+</div>
---
+++ sites/all/themes/sky/templates/toto5.tpl.php
@@ -0,0 +1,4 @@
+
+<div style="color:blue">
+ myvar_tpl and (<?php print $variables['myvar']; ?> or <?php print $variables['myvar2']; ?>)
+</div>
